d6254426a57608798b3936f8d4aa9f9bdadf20c0
Pristajchuk-Humain <73787800+SvitlanaPristajchuk@users.noreply.github.com>
2022-11-07 16:13:29 +0100

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-//const ticket = true;
-//const plane = true;
-const sertificate = 0;
-
-//const result= true;
-//console.log('result', ticket && plane && sertificate); 
-//console.log('result', ticket || plane || sertificate); 
-console.log(sertificate); 
