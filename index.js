bd72b76d45708cd589a18693939223ed251676e0
Pristajchuk-Humain <73787800+SvitlanaPristajchuk@users.noreply.github.com>
2022-11-07 15:52:23 +0100

    lessons_1

---
+++ index.js
@@ -0,0 +1,8 @@
+//const ticket = true;
+//const plane = true;
+const sertificate = 0;
+
+//const result= true;
+//console.log('result', ticket && plane && sertificate); 
+//console.log('result', ticket || plane || sertificate); 
+console.log(sertificate); 
