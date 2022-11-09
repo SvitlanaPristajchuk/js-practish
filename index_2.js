1c6be6930d1d9daae3e5000db4a58f30f18a8cba
Pristajchuk-Humain <73787800+SvitlanaPristajchuk@users.noreply.github.com>
2022-11-09 06:29:14 +0100

    lessons_2

---
+++ index_2.js
@@ -0,0 +1,7 @@
+ console.log(null && 'false' && 'Manh');
+ console.log (null || 'false' || 'Mnn');
+ console.log('false' && 'MAbn' || nul);
+ console.log ('Mafjh' && null || false);
+
+
+ console.log()
