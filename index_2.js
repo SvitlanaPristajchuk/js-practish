71c449e524681168994f4ca3a6d28accc142a153
Svetlana Pristaychuk <svetlana.pristajchuk@gmail.com>
2022-12-03 12:18:35 +0100

    add

--- index_2.js
+++ index_2.js
@@ -1,22 +1,3 @@
 
 
 
-
-const productName = "Repair droid";
-
-
-function formatMessage(message, maxLength) {
-  let result;
-  const newMessage = message.slice(maxLength);
-  const continie = '...'
-  // Change code below this line
-if (message.lenght >= maxLength) {
-  result = message;
-} else {
-    result = newMessage;
-}
-
-return result
-}
- 
-console.log(result)
